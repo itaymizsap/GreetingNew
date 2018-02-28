7907567782c336cd72a9cc6b629b2e1430993237
itaymizsap@Optics1 <itaymizsap@gmail.com>
2018-02-28 10:40:32 +0200

    fixing application.properties MySQL params

---
+++ GreetingsMySqlService/src/main/java/com/itay/spring/tutorials/microservices/greeting/mysql/AppMainSpringMySql.java
@@ -0,0 +1,11 @@
+package com.itay.spring.tutorials.microservices.greeting.mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMainSpringMySql {
+    public static void main(String[] args) {
+        SpringApplication.run(AppMainSpringMySql.class, args);
+    }
+}
